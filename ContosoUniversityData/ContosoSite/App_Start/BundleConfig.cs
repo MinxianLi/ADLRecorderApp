ecacab86bc87d81694a60c334235a53392551873
MinxianLi <lmx423@gmail.com>
2016-08-09 23:15:57 -0400

    Add Chart.js

--- ContosoUniversityData/ContosoSite/App_Start/BundleConfig.cs
+++ ContosoUniversityData/ContosoSite/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace ContosoSite
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/chart").Include(
+                     "~/Scripts/Chart.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
