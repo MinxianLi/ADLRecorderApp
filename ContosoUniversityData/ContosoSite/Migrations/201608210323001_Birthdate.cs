07be8148d84f3863b869f56f24dc2c69bf1f70c2
MinxianLi <lmx423@gmail.com>
2016-08-20 23:57:37 -0400

    hidden table

---
+++ ContosoUniversityData/ContosoSite/Migrations/201608210323001_Birthdate.cs
@@ -0,0 +1,18 @@
+namespace ContosoSite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Birthdate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "BirthDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "BirthDate");
+        }
+    }
+}
